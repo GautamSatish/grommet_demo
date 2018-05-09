fec7af46c425224794120e71a285c6c1fd1963b3
Gautam Satish <gautams@hpe.com>
2018-05-09 12:10:14 +0530

    Updated to use Root component.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import 'grommet/scss/vanilla/index.scss';
 
-import store from './store';
-import Demo from './components/Demo';
+import Root from './components/Root';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <Demo />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
